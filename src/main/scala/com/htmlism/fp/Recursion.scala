6d6e88d8018c913645c1f7d5bbc6c149511d5718
Mark Canlas <mark.canlas@tapad.com>
2017-06-12 19:10:34 -0400

    added recursion doc

--- src/main/scala/com/htmlism/fp/Recursion.scala
+++ src/main/scala/com/htmlism/fp/Recursion.scala
@@ -2,6 +2,16 @@ package com.htmlism.fp
 
 import scala.annotation.tailrec
 
+/**
+  * Some languages have an optimization called "tail recursion". This optimization allows functions to be written in a
+  * recursive style and be executed without incurring the cost of a deep execution stack for long inputs.
+  *
+  * Notice how the recursive case for this function consists only of a call to the function, without any outer
+  * expression. A compiler can flatten this recursion into a loop since the value for the recursive case can be
+  * thought of as a new, isolated function each time.
+  *
+  * The `@tailrec` annotation allows Scala to assert that this function can be optimized as tail recursive.
+  */
 object Recursion extends App {
   val numbers = Seq(86, 44, 62, 77, 25)
 
@@ -18,6 +28,10 @@ object Recursion extends App {
   println(sum(numbers))
 }
 
+/**
+  * This is how a compiler might unravel the tail recursion above into a `while` loop.
+  */
+
 object RecursionWhile extends App {
   var numbers = Seq(86, 44, 62, 77, 25)
 
@@ -26,9 +40,6 @@ object RecursionWhile extends App {
   while (numbers.nonEmpty) {
     acc = acc + numbers.head
     numbers = numbers.tail
-
-    if (numbers.isEmpty)
-      acc = Recursion.sum(numbers, acc)
   }
 
   println(acc)
--- src/main/scala/com/htmlism/oop/Recursion.scala
+++ src/main/scala/com/htmlism/oop/Recursion.scala
@@ -1,5 +1,14 @@
 package com.htmlism.oop
 
+/**
+  * Recursion exists in object-oriented programming, but isn't used often. During execution, the depth of the
+  * execution stack is directly proportional to the length of the input, making style of coding unsuitable for
+  * large inputs.
+  *
+  * A recursion function will often have two parts: the base case and the recursive case. Notice how the recursive call
+  * is a part of the overall expression that is the recursive case. The value for the recursive case cannot be known
+  * unless all of the calls inside of it have resolved.
+  */
 object Recursion extends App {
   val numbers = Seq(86, 44, 62, 77, 25)
 
